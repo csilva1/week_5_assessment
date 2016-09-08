b33a36e9bc872e99350c62cc5248f69a9c874db0
Carol Silva <Carol@carols-air.attlocal.net>
2016-09-07 22:41:28 -0700

    Add empty array and use While

--- calculator.rb
+++ calculator.rb
@@ -20,19 +20,13 @@ end
 
 calc_h = {}
 while true
-puts "What would you want to calculate?"
-user_string = gets.chomp
+puts "What would you want to calculate? Enter the first integer:"
+int1 = gets.chomp.to_i
 
- str = user_string.split(//)
- ind_1 = str[0].to_i 
- ind_2 = str[1]
- ind_3 = str[2].to_i
-
-	p calculate(ind_1, ind_2, ind_3)
 # puts calculate(4, '+', 5)
 # puts calculate(3, '-', 1)
 # puts calculate(5, '*', 5)
 # puts calculate(49, '/', 7)
-\
+
 
 
